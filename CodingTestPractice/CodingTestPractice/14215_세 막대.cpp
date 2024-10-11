ba31c21c215c92c1736a0fd90b2ad8bba9527db4
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2024-10-11 13:42:53 +0900

    11일차

--- CodingTestPractice/CodingTestPractice/14215_세 막대.cpp
+++ CodingTestPractice/CodingTestPractice/14215_세 막대.cpp
@@ -2,6 +2,34 @@
 
 int main()
 {
+	int a[3] = { 0 };
+	scanf_s("%d %d %d", &a[0], &a[1], &a[2]);
+
+	if (a[0] == a[1] && a[1] == a[2] && a[2] == a[0])
+	{
+		printf("%d", a[0] + a[1] + a[2]);
+		return 0;
+	}
+
+	int max = 0, index;
+	for (int i = 0; i < 3; ++i)
+	{
+		if (max < a[i])
+		{
+			max = a[i];
+			index = i;
+		}
+	}
+
+	int sum = 0;
+	for (int i = 0; i < 3; ++i)
+	{
+		if (index == i) continue;
+
+		sum += a[i];
+	}
+
+	printf("%d", sum > max ? sum + a[index] : sum + sum - 1);
 
 	return 0;
 }
