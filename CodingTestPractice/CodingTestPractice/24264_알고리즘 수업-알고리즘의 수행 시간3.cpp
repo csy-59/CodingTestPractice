d808b8dd705b923693f61f18594833d97365ee62
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2024-10-20 18:33:01 +0900

    13, 14일차

--- CodingTestPractice/CodingTestPractice/24264_알고리즘 수업-알고리즘의 수행 시간3.cpp
+++ CodingTestPractice/CodingTestPractice/24264_알고리즘 수업-알고리즘의 수행 시간3.cpp
@@ -1,17 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
+using namespace std;
 int main()
 {
 	long long n;
-	scanf_s("%ll", &n);
-
-	if (n == 1)
-	{
-		printf("%ll\n0", n * n);
-		return 0;
-	}
-
-	printf("%ll\n2", n * n);
+	cin >> n;
+	cout << n * n << "\n2";
 
 	return 0;
 }
